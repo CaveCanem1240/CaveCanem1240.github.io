0115f60851c95ef4cc81fe07a69f462b9495576e
Cave Canem <523048190@qq.com>
2021-08-21 19:41:13 +0800

    home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,17 +44,19 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
-
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 
-window.addEventListener('', )
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL TOP ====================*/ 
 function scrollTop() {
-
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) nav.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
 }
 
-window.addEventListener('', )
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
 //const themeButtom =
