1d0de5412b17ec371449d05d792af1cc4a305991
Cave Canem <523048190@qq.com>
2023-11-19 23:43:24 -0500

    regular update

--- js/script.js
+++ js/script.js
@@ -34,3 +34,10 @@ document.querySelectorAll('a').forEach(links =>{
     }
 
 });
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
